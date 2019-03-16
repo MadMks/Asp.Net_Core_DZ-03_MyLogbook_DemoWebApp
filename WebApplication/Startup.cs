33ebe51f0baef8df0c2d3cf5d96a878876c23b13
Makc <mks59k@gmail.com>
2019-03-16 13:42:51 +0200

    Отключение https.

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -54,7 +54,7 @@ namespace WebApplication
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
             using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
@@ -66,7 +66,7 @@ namespace WebApplication
                 context.Database.EnsureCreated();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
