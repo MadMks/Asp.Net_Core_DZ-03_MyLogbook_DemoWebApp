ba65dc4685a7a8bda513acea93c5c0c96bfbeb4c
Makc <mks59k@gmail.com>
2019-03-16 14:33:14 +0200

    Добавлена модель оценок.

---
+++ WebApplication/Models/Mark.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Entities;
+
+namespace WebApplication.Models
+{
+    [Table("marks")]
+    public class Mark
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        public virtual TeacherSubject TeacherSubject { get; set; }
+        public virtual Student Student { get; set; }
+
+        [Column("value")]
+        public int Value { get; set; }
+    }
+}
